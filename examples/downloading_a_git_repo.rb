7b8bf35aaa70a412ca602e68086b81dfebe6f34a
nakahira <1021057927@qq.com>
2021-12-31 07:51:47 +0800

    add note

--- examples/downloading_a_git_repo.rb
+++ examples/downloading_a_git_repo.rb
@@ -2,9 +2,17 @@ $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 
 require 'cocoapods-downloader'
 
-path = ''
-options = {}
-downloader = Pod::Downloader.for_target(path, options)
-downloader.cache_root = ''
-downloader.max_cache_size = 500
+# path = ''
+# options = {}
+# downloader = Pod::Downloader.for_target(path, options)
+# downloader.cache_root = ''
+# downloader.max_cache_size = 500
+# downloader.download
+#
+
+target_path = File.expand_path('../../tmp/download', __FILE__)
+options = {:git => 'https://github.com/square/cocoapods-pack.git'}
+options = Pod::Downloader.preprocess_options(options)
+downloader = Pod::Downloader.for_target(target_path, options)
 downloader.download
+downloader.checkout_options
